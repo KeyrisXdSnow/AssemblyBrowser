91bc7e1240a10116b85d43309b63f8a16c4a3f2e
Keyris <KeyrisXdSnow>
2020-10-28 22:42:28 +0300

    add MVVM pattern

---
+++ View/OpenFileCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace View
+{
+    internal class OpenFileCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<object, bool> _canExecute;
+        
+        public OpenFileCommand(Action execute, Func<object, bool> canExecute = null)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            //\_execute(parameter);
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+}
